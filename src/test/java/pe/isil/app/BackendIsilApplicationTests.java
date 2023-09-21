af8ac51796425d334116e4864560157e2f5f4659
gonza <gonzadevelw@gmail.com>
2023-09-21 13:33:54 -0500

    setup backend

---
+++ src/test/java/pe/isil/app/BackendIsilApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIsilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
